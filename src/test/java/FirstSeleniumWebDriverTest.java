f4523a0514ebd4b34ba09b97ab6374039dd5f3ae
agrandaa <agn.granda@gmail.com>
2023-02-20 11:30:04 +0100

    first commit

--- src/test/java/FirstSeleniumWebDriverTest.java
+++ src/test/java/FirstSeleniumWebDriverTest.java
@@ -1,20 +1,39 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import java.time.Duration;
 
 public class FirstSeleniumWebDriverTest {
 
+    WebDriver driver = new ChromeDriver();
+
     @Test
-    public void simpleSeleniumTest() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    public void sampleTest() {
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
         String title = driver.getTitle();
         Assert.assertEquals("Web form", title);
-        System.out.println("Title: " + title);
 
+        WebElement color = driver.findElement(By.xpath("//input[@name='my-colors']"));
+        Assert.assertEquals("#563d7c", color.getAttribute("value"));
+
+        WebElement textInput = driver.findElement(By.name("my-text"));
+        textInput.sendKeys("text");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        Assert.assertEquals(message.getText(), "Received!");
+    }
+
+    @After
+    public void quitDriver(){
+        driver.close();
         driver.quit();
     }
 }
